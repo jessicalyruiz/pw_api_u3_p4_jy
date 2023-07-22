2940dca8bd0ad683465b25978ac51fb835e1ed7e
jessicalyruiz <jessicalyr@gmail.com>
2023-07-22 10:43:15 -0500

    nivel3

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFull.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.repository.model.Estudiante;
@@ -29,11 +32,12 @@ public class EstudianteControllerRestFull {
 
 	IEstudianteService estudianteService; 
 
-	@GetMapping(path = "/{cedula}") 
+	//@GetMapping(path = "/{cedula}", produces = MediaType.APPLICATION_JSON_VALUE) 
+	@GetMapping(path = "/{cedula}", produces = "application/xml")
+	@ResponseStatus(code = HttpStatus.CONFLICT) //estos prefereible usar en los 300
+	public Estudiante consultarCedula(@PathVariable String cedula) { 
 
-	public ResponseEntity<Estudiante> consultarCedula(@PathVariable String cedula) { 
-
-	return ResponseEntity.status(240).body(this.estudianteService.buscarCedula(cedula)); 
+	return this.estudianteService.buscarCedula(cedula);
 
 	} 
 
@@ -47,13 +51,28 @@ public class EstudianteControllerRestFull {
 
 	} 
 
-	@PostMapping  //no hace falta el identificador.. solo sse inserta 1 registro 
+	@PostMapping(consumes = "application/json")  //no hace falta el identificador.. solo sse inserta 1 registro 
 
 	public void ingresarEstudiante(@RequestBody Estudiante estudiante) { 
 
 	this.estudianteService.create(estudiante); 
 
 	} 
+	
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_XML_VALUE)  //no hace falta el identificador.. solo sse inserta 1 registro 
+
+	public Estudiante ingresarEstudianteConsumesProd(@RequestBody Estudiante estudiante) { 
+
+	this.estudianteService.create(estudiante); 
+	
+	return this.estudianteService.buscarCedula(estudiante.getCedula());
+		
+	}
+	
+	
+	
+	
 
 	@PutMapping(path = "/{identif}") 
 
